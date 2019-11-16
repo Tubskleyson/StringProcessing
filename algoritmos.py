79a228d1c6516b8d864a222ca94f03f8d91eaa28
Tubs <tubskleyson@gmail.com>
2019-11-16 16:13:44 -0300

    aloritmos prontos

--- algoritmos.py
+++ algoritmos.py
@@ -2,16 +2,17 @@
 Algoritmos de casamento exato
 """
 
-def forca_bruta(T, n, P, m):
+def forca_bruta(T, P):
 
     """
     Realiza a busca pelo método de força brura
     :param str T:
-    :param int n:
     :param str P:
-    :param int m:
     :return int[]:
     """
+
+    m = len(P)
+    n = len(T)
 
     r = []
 
@@ -29,16 +30,17 @@ def forca_bruta(T, n, P, m):
     return r
 
 
-def bmh(T, n, P, m):
+def bmh(T, P):
 
     """
     Realiza a busca pelo método Boyer-Moore-Horspool
     :param str T:
-    :param int n:
     :param str P:
-    :param int m:
     :return int[]:
     """
+
+    m = len(P)
+    n = len(T)
 
     r, d = [], [m for _ in range(256)]
 
@@ -61,16 +63,17 @@ def bmh(T, n, P, m):
 
     return r
 
-def bmhs(T, n, P, m):
+def bmhs(T, P):
 
     """
     Realiza a busca pelo método Boyer-Moore-Horspool-Sunday
     :param str T:
-    :param int n:
     :param str P:
-    :param int m:
     :return int[]:
     """
+
+    m = len(P)
+    n = len(T)
 
     r, d = [], [ m + 1 for _ in range(256)]
 
@@ -92,3 +95,96 @@ def bmhs(T, n, P, m):
         i += d[ ord(T[ i + 1 ]) ]
 
     return  r
+
+
+def shift_and(P, T):
+
+    """
+    Realiza a busca por meio de shift-and exato
+    :param str P:
+    :param str T:
+    :return int[]:
+    """
+
+    r = []
+
+    m = len(P)
+    n = len(T)
+
+    alf = []| 2**(m-1)
+    M = {}
+
+    for i in T:
+
+        if i not in alf: alf.append(i)
+
+    for c in alf: M[c] = 0
+
+    for j in range(m):
+        M[P[j]] = M[P[j]] | 2**(m-j-1)
+
+
+    R = 0
+
+    for i in range(n):
+
+        R = ((R >> 1) | 2 ** (m-1)) & M[T[i]]
+
+        if R & 1 != 0 :r.append(i - m + 1)
+
+    return r
+
+def shift_and_aprox(P, T, k):
+
+    """
+    Realiza a busca por meio de shift-and exato
+    :param str P:
+    :param str T:
+    :param int k:
+    :return int[]:
+    """
+
+    r = []
+
+    m = len(P)
+    n = len(T)
+
+    alf = []
+    M = {}
+
+    for i in T:
+
+        if i not in alf: alf.append(i)
+
+    for c in alf: M[c] = 0
+
+    for j in range(m):
+        M[P[j]] = M[P[j]] | 2**(m-j-1)
+
+
+    R = [0 for _ in range(k+1)]
+
+    for j in range(k+1):
+        exec(f"R[j] = 0b{'1'*j}{'0'*(m-j)}")
+
+    for i in range(n):
+
+        Ra = R[0]
+
+        Rn =  ((Ra >> 1) | 2 **(m-1)) & M[T[i]]
+
+        R[0] = Rn
+
+        for j in range(1,k+1):
+
+            Rn = ((R[j] >> 1) & M[T[i]]) | Ra | ((Ra | Rn) >> 1)
+
+            Ra= R[j]
+
+            R[j] = Rn | 2**(m-1)
+
+        if Rn & 1 != 0 :r.append(i)
+
+    return r
+
+
